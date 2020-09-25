3bfb843d9c106c6fe378eee50cec40f6e783c2a4
Joseph Kniskern <kniskernjoseph@yaho.com>
2020-09-25 12:05:10 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+array1 = np.array([1, 2, 3])
+array2 = np.zeros((3, 4), dtype=int)
+array3 = np.ones((3, 4), dtype=int)
+array4 = np.full((3, 4), 33, dtype=int)
+array5 = np.random.random((5, 5))
+
+print(array5)
